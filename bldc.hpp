37009c72d1fe7d7454ce5c7d69fe5628865e35a2
aaryanmakwana <aaryanmakwana2345240@gmail.com>
2025-11-28 11:18:41 +0530

    update

--- bldc.hpp
+++ bldc.hpp
@@ -7,13 +7,25 @@
 using namespace std;
 
 #define Kw 0.955
+#define PI 3.14159265358979323846
+#define f 50
+#define nPHASES 3
+#define RESISTIVITY 0.0000002
+#define SPECIFIC_IRON_LOSS 0.02
 
 unsigned seed;
+
+struct fitness_function{
+    double W_eff;
+    double W_T;
+    double W_op;
+};
 
 struct bldc_params{
   double P;
   double RPM;
   double V;
+  double efficiency;
 };
 
 struct design_variables{
@@ -27,6 +39,7 @@ struct design_variables{
     double lg;
     int Nspp;
     double Lt_ratio;
+    double Kmag;
 };
 
 struct bldc_design{
@@ -40,6 +53,9 @@ struct bldc_design{
     double L;
     double drt;
     double Wry;
+    double d_mag;
+    double mag_arc;
+    double Tphase;
 };
 
 struct performance_params{
@@ -49,6 +65,10 @@ struct performance_params{
     double copper_loss;
     double iron_loss;
 };
+
+double random_double(double, double);
+void design_variables(class bldc);
+int random_int(int,int);
 
 double random_double(double lower, double upper) {
     int rd = rand();
@@ -73,6 +93,7 @@ class bldc{
         parameters.V = params.V;
         parameters.P = params.P;
         parameters.RPM = params.RPM;
+        parameters.efficiency = params.efficiency;
     }
 
     void guess_variables(){
@@ -86,7 +107,114 @@ class bldc{
         variables.J = random_double(2.5, 5.5);
         variables.Nspp = random_int(1, 4);
         variables.Lt_ratio = random_double(0.5, 2.0);
-
-        std::cout << variables.Bg << "\n";
+        variables.Kmag = random_double(0.5, 1.0);
+        //print_variables();
     }
+
+    void print_variables(){
+        std::cout << "Bg : " << variables.Bg << ",\t";
+        std::cout << "Bst : " << variables.Bst << ",\t";
+        std::cout << "Bsy : " << variables.Bsy << ",\t";
+        std::cout << "Brt : " << variables.Brt << ",\t";
+        std::cout << "Bry : " << variables.Bry << ",\t";
+        std::cout << "ac : " << variables.ac << ",\t";
+        std::cout << "lg : " << variables.lg << ",\t";
+        std::cout << "J : " << variables.J << ",\t";
+        std::cout << "Nspp : " << variables.Nspp << ",\t";
+        std::cout << "Lt_ratio : " << variables.Lt_ratio << "\t";
+    }
+
+    void print_performance(){
+        std::cout << "efficiency : " << performance.efficiency << ",\t";
+        std::cout << "copper_loss : " << performance.copper_loss << ",\t";
+        std::cout << "torque : " << performance.torque << ",\t";
+        std::cout << "iron_loss : " << performance.iron_loss << ",\t";
+        std::cout << "current : " << performance.current << "\t";
+    }
+
+    void print_all(){
+        std::cout<< "[ ";
+        print_variables();
+        std::cout<< ",\t";
+        print_performance();
+        std::cout<< " ]\n";
+    }
+
+    double fitness(struct fitness_function ff){
+        return ((ff.W_T * performance.torque) + (ff.W_eff * performance.efficiency) + (ff.W_op * performance.current * parameters.V))/(ff.W_eff + ff.W_op + ff.W_T);
+    }
+
 };
+
+void design_bldc(class bldc * bldc){
+    double Ktrv = 2 * bldc->variables.ac * bldc->variables.Bg;
+    double T = (60 * bldc->parameters.P) / (bldc->parameters.efficiency * bldc->parameters.RPM * 2 * PI);
+    double nPoles = 120 * f / bldc->parameters.RPM;
+    double Ld_ratio = bldc->variables.Lt_ratio * PI / nPoles;
+
+    bldc->design.Dro = std::pow(4*T/(PI * Ktrv * Ld_ratio), (1/3));
+    bldc->design.L = bldc->design.Dro * Ld_ratio;
+
+    double Nslots = bldc->variables.Nspp * nPHASES * nPoles;
+
+    bldc->performance.current = (bldc->parameters.P * 1000) / (bldc->parameters.efficiency * bldc->parameters.V * 2);
+
+    bldc->design.Tphase = (int)(PI*bldc->design.Dro*bldc->variables.ac) / (int)( 1000 * bldc->performance.current);
+    int Z = 2*bldc->design.Tphase;
+
+    double Ac = bldc->performance.current / bldc->variables.J;
+    double Aslot = Ac*Z;
+
+    bldc->design.Wst = (bldc->variables.Bg * PI * bldc->design.Dro / nPoles) / (bldc->variables.Bst * bldc->design.L);
+    double Wslot = (PI * bldc->design.Dro / Nslots) - bldc->design.Wst;
+    double d_slot = Aslot/Wslot;
+
+    bldc->design.Wsy = (bldc->variables.Bg * PI * bldc->design.Dro / nPoles) / (2 * bldc->variables.Bsy * bldc->design.L);
+    bldc->design.d_mag = bldc->variables.lg * random_double(5.0, 11.0);
+
+    bldc->design.Dso = bldc->design.Dro + (2*bldc->design.Wsy) + (2*d_slot);
+    bldc->design.mag_arc = (360/nPoles) * bldc->variables.Kmag;
+    bldc->design.Wry   = (bldc->variables.Bg * PI * bldc->design.Dro / nPoles) / (2 * bldc->variables.Bry * bldc->design.L);
+    bldc->design.Dsh = bldc->design.Dro - (2*bldc->design.d_mag) - (2*bldc->design.Wry);
+
+    bldc->performance.torque = T;
+    /*
+    while (bldc->design.Dsh < 0) {
+        std::cout << "Design failed, retrying..." << std::endl;
+        bldc->design.d_mag *= 0.9;
+        bldc->design.Wry *= 0.9;
+    }
+    */
+}
+
+void estimate_performance(class bldc * bldc) {
+    bldc->performance.copper_loss = std::pow(bldc->variables.ac * bldc->design.Dro * PI, 2) * (bldc->design.Tphase * bldc->design.L * nPHASES * RESISTIVITY);
+    bldc->performance.iron_loss = 0.25*PI*(std::pow(bldc->design.Dso, 2) - std::pow(bldc->design.Dro, 2)) * bldc->design.L * SPECIFIC_IRON_LOSS;
+    double power = 3 * bldc->parameters.V * bldc->performance.current;
+    bldc->performance.efficiency = power / (power + bldc->performance.copper_loss + bldc->performance.iron_loss);
+    //bldc->print_all();
+
+    /*
+    std::cout << "[" ;
+    bldc->print_performance();
+    std::cout << "]\n" ;
+    */
+}
+
+
+bldc breed(bldc parent1, bldc parent2) {
+    bldc child;
+    child.parameters = parent1.parameters;
+    child.variables.Bg = (rand()%2 == 0) ? parent1.variables.Bg : parent2.variables.Bg;
+    child.variables.Bst = (rand()%2 == 0) ? parent1.variables.Bst : parent2.variables.Bst;
+    child.variables.Bsy = (rand()%2 == 0) ? parent1.variables.Bsy : parent2.variables.Bsy;
+    child.variables.Brt = (rand()%2 == 0) ? parent1.variables.Brt : parent2.variables.Brt;
+    child.variables.Bry = (rand()%2 == 0) ? parent1.variables.Bry : parent2.variables.Bry;
+    child.variables.J = (rand()%2 == 0) ? parent1.variables.J : parent2.variables.J;
+    child.variables.Kmag = (rand()%2 == 0) ? parent1.variables.Kmag : parent2.variables.Kmag;
+    child.variables.Lt_ratio = (rand()%2 == 0) ? parent1.variables.Lt_ratio : parent2.variables.Lt_ratio;
+    child.variables.Nspp = (rand()%2 == 0) ? parent1.variables.Nspp : parent2.variables.Nspp;
+    child.variables.ac = (rand()%2 == 0) ? parent1.variables.ac : parent2.variables.ac;
+    child.variables.lg = (rand()%2 == 0) ? parent1.variables.lg : parent2.variables.lg;
+    return child;
+}
--- main.cpp
+++ main.cpp
@@ -1,16 +1,95 @@
 #include "bldc.hpp"
+#include <iostream>
+#include <thread>
 
 #define N_CHIDREN 10
 #define N_GENERATION 100
+#define THREAD_MAX 8
+#define N_TOPPERS 3
+
+void get_new_generation(bldc* BLDC);
+bldc_params params = {50.00,1500.00,415.00,0.95};
+fitness_function fitness_func = {15,1,10};
 
 int main(){
-    bldc_params params = {50.00,1500.00,415.00};
 
     bldc BLDC[N_CHIDREN];
+    srand(time(0));
+
     for (int i=0;i<N_CHIDREN;i+=1) {
-        srand(time(0));
         BLDC[i].init(params);
         BLDC[i].guess_variables();
     }
+
+    std::thread threads[N_CHIDREN];
+
+    for(int j=0;j<N_GENERATION;j+=1){
+        std::cout << "Generation : " << j << "\n";
+        //std::cout << "\tDesigning BLDC motors" << std::endl;
+        for (int i=0;i<N_CHIDREN;i+=1) {
+            design_bldc(&BLDC[i]);
+        }
+        //std::cout << "\tDesign complete" << std::endl;
+        //std::cout << "\tEstimating performance" << std::endl;
+        double eff_avg = 0;
+        double eff_max = 0;
+        for (int i=0;i<N_CHIDREN;i+=1) {
+            estimate_performance(&BLDC[i]);
+            eff_avg += BLDC[i].performance.efficiency;
+            if (BLDC[i].performance.efficiency > eff_max) {
+                eff_max = BLDC[i].performance.efficiency;
+            }
+        }
+        eff_avg /= N_CHIDREN;
+        std::cout << "\tAverage efficiency: " << eff_avg << "\n";
+        std::cout << "\tMaximum efficiency: " << eff_max << "\n\n";
+        //std::cout << "\tPerformance estimation complete" << std::endl;
+        //std::cout << "\tSelecting top performers\n" << std::endl;
+        get_new_generation(BLDC);
+    }
     return 0;
+}
+
+void get_new_generation(bldc* BLDC){
+    bldc* new_BLDC = new bldc[N_CHIDREN];
+    for (int i=0;i<N_CHIDREN;i+=1) {
+        new_BLDC[i].init(params);
+        new_BLDC[i].guess_variables();
+    }
+
+    double fitness;
+    double max_fitness[N_TOPPERS] = {0.0,0.0,0.0};
+    int max_indexes[N_TOPPERS] = {-1,-1,-1};
+
+    for (int i=0;i<N_CHIDREN;i+=1) {
+        fitness = BLDC[i].fitness(fitness_func);
+        if (fitness > max_fitness[0]) {
+            max_fitness[2] = max_fitness[1];
+            max_fitness[1] = max_fitness[0];
+            max_fitness[0] = fitness;
+            max_indexes[2] = max_indexes[1];
+            max_indexes[1] = max_indexes[0];
+            max_indexes[0] = i;
+        } else if (fitness > max_fitness[1]) {
+            max_fitness[2] = max_fitness[1];
+            max_fitness[1] = fitness;
+            max_indexes[2] = max_indexes[1];
+            max_indexes[1] = i;
+        } else if (fitness > max_fitness[2]) {
+            max_fitness[2] = fitness;
+            max_indexes[2] = i;
+        }
+    }
+
+    new_BLDC[0] = breed(BLDC[max_indexes[0]], BLDC[max_indexes[1]]);
+    new_BLDC[1] = breed(BLDC[max_indexes[1]], BLDC[max_indexes[2]]);
+    new_BLDC[2] = breed(BLDC[max_indexes[2]], BLDC[max_indexes[0]]);
+    new_BLDC[3] = BLDC[max_indexes[0]];
+    new_BLDC[4] = BLDC[max_indexes[1]];
+    new_BLDC[5] = BLDC[max_indexes[2]];
+
+    for (int i=0;i<N_CHIDREN;i+=1) {
+        BLDC[i] = new_BLDC[i];
+    }
+
 }
